7b7555cd3e05bf5ec594e486dcf5983c3ec7b6f4
Rodrigo Golfeto <rgolfeto@gmail.com>
2023-04-01 01:47:06 -0300

    New Layout

--- index.php
+++ index.php
@@ -21,51 +21,32 @@ $mpdf = new \Mpdf\Mpdf($mpdfConfig);
 $mpdf->SetTitle('Techlex | Conteúdo digital sobre rotinas trabalhistas');
 $mpdf->WriteHTML(file_get_contents(__DIR__ . '/css/styles.css'), \Mpdf\HTMLParserMode::HEADER_CSS);
 
+$local_data_hora = 'Cuiabá, 30 de março de 2023.';
+$conteudo = 'Concluiu com aproveitamento a Capacitação sobre Assédio Moral, Assédio Sexual e Demais Violências no Ambiente de Trabalho, com carga horária de 2 (duas) horas, em atendimento à Lei n. 14.457/2022, NR 01 e Anexos.';
 
-################################################################################
-// PAGINA 1
-################################################################################
-$mpdf->AddPage('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'one');
-$mpdf->WriteHTML('
-<div style="width:100%;height:100%;color:#424242;">
-  
-  <div style="padding:330px 75px 0 75px;">
-    <div style="width:100%;text-align:center;font-size:20px;color:#542872;line-height: 1.4;font-weight:bold;">Concluiu com aproveitamento a Capacitação sobre Assédio Moral, Assédio Sexual e Demais Violências no Ambiente de Trabalho, com carga horária de 2 (duas) horas, em atendimento à Lei n. 14.457/2022, NR 01 e Anexos.</div>
-  </div>
-  
-  <div style="padding:40px 75px 0 75px;">
-    <div style="width:100%;text-align:center;font-size:18px;color:#542872;line-height: 1.4;font-weight:normal;">Cuiabá, 30 de março de 2023.</div>
-  </div>
+$alunos_array = [
+  'Murilo Victor Filipe Moura',
+  'Olivia Eloá Ester Caldeira',
+  'Sebastião Antonio Diego da Paz',
+  'Débora Luana Nogueira'
+];
 
-  <div style="padding:40px 115px 0 115px;">
-    <table style="width:100%;text-align:center;">
-      <tr>
-        <td width="205px">Rodrigo Golfeto de Queiroz</td>
-        <td width=""></td>
-        <td width="205px"><img src="images/assinatura-1.png" width="100px" /></td>
-        <td width=""></td>
-        <td width="205px"><img src="images/assinatura-2.png" width="100px" /></td>
-      </tr>
-      <tr>
-        <td width="205px" style="color:#5F3D7D;border-top:1px solid #7F4A9D;padding:5px 0 0 0;">Aluno</td>
-        <td width=""></td>
-        <td width="205px" style="color:#5F3D7D;border-top:1px solid #7F4A9D;padding:5px 0 0 0;">Engº(a) do Trabalho</td>
-        <td width=""></td>
-        <td width="205px" style="color:#5F3D7D;border-top:1px solid #7F4A9D;padding:5px 0 0 0;">Instrutora Tech Lex</td>
-      </tr>
-    </table>
-  </div>
+foreach($alunos_array as $item){
 
-</div>
-');
-################################################################################
+  ################################################################################
+  // PAGINA 1
+  ################################################################################
+  $mpdf->AddPage('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'one');
+  $mpdf->WriteHTML('<div style="width:100%;height:100%;color:#424242;"><div style="padding:250px 115px 0 115px;"><table style="width:100%;text-align:center;"><tr><td style="color:#5F3D7D;font-size:26px;font-weight:bold;">' . $item . '</td></tr></table></div><div style="padding:20px 75px 0 75px;"><div style="width:100%;text-align:center;font-size:20px;color:#542872;line-height: 1.4;font-weight:bold;">' . $conteudo . '</div></div><div style="padding:30px 75px 0 75px;"><div style="width:100%;text-align:center;font-size:18px;color:#542872;line-height: 1.4;font-weight:normal;">' . $local_data_hora . '</div></div></div>');
+  ################################################################################
 
-################################################################################
-// PAGINA 2
-################################################################################
-$mpdf->AddPage('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'two');
-$mpdf->WriteHTML('');
-################################################################################
+  ################################################################################
+  // PAGINA 2
+  ################################################################################
+  $mpdf->AddPage('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'two');
+  $mpdf->WriteHTML('');
+  ################################################################################
 
+}
 
-$mpdf->Output();
+$mpdf->Output('certificado-techlex.pdf', 'I');
